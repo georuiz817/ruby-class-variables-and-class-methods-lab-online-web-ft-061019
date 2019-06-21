19cabc8c94806e8b76f0f057200395012da20793
George Ruiz <georuiz817@gmail.com>
2019-06-21 23:42:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,4 +21,4 @@ end
 ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "Rap")
 mask_off = Song.new("Mask Off", "Future", "Rap")
 dance_macabre = Song.new("Dance Macabre", "Ghost", "Metal")
-if_you_have_ghosts = Song.new("If You Have Ghosts", "Ghost", "Metal")
+ 
