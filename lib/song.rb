1e1ea072a3f9274a09bff750a4b3aca9f76515ad
George Ruiz <georuiz817@gmail.com>
2019-06-21 23:40:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
  
 attr_accessor :name, :artist, :genre
 
-@@song_count = 0
+@@count = 0
 
 
 def initialize(name, artist, genre) 
@@ -13,7 +13,7 @@ end
 
  
   def self.count 
-    @@song_count
+    @@count
   end
 
  
