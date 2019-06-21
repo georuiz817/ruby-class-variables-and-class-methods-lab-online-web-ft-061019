ecc95c761b03a7ecd44bbdc3d9f7d0c2155228ad
George Ruiz <georuiz817@gmail.com>
2019-06-21 23:53:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,13 +4,14 @@ attr_accessor :name, :artist, :genre
 
 @@count = 0 
 @@artists = []
-
+@@genres = []
 def initialize(name, artist, genre) 
 @name = name 
 @artist = artist
 @genre = genre
 @@count += 1
 @@artists << @artist 
+@@genres << @genre 
 end
 
  
